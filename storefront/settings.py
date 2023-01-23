fd070140e7bad8b7ecf89bad0590db56103b1d82
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2023-01-23 23:31:32 +0000

    Installing and configuration Django REST framework

--- storefront/settings.py
+++ storefront/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sessions',
     'debug_toolbar',
+    'rest_framework',
     'products'
 ]
 
--- storefront/urls.py
+++ storefront/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('products/', include('products.urls'))
 ]
